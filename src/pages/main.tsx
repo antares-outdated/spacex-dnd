8613de26e5e38565bfe35c9ecb1e5d9414942f25
antaresofficial <antaresofficial@icloud.com>
2022-03-17 14:10:56 +0500

    Update main.tsx

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -48,10 +48,23 @@ export const Main: React.FC = () => {
   };
 
   useEffect(() => {
-    request("launches?limit=5").then((result) => {
-      dispatch(updateColumns(createColumns(result)));
-    });
+    console.log(localStorage.getItem("spacex"));
+    if (localStorage.getItem("spacex")) {
+      const newColumns = createColumns(
+        JSON.parse(localStorage.getItem("spacex") || "{}")
+      );
+      // dispatch(updateColumns(newColumns));
+      console.log(newColumns);
+    } else {
+      request("launches?limit=5").then((result) => {
+        dispatch(updateColumns(createColumns(result.slice(0, 10))));
+      });
+    }
   }, []);
+
+  // useEffect(() => {
+  //   localStorage.setItem("spacex", JSON.stringify(columns));
+  // }, [columns]);
 
   return (
     <>
